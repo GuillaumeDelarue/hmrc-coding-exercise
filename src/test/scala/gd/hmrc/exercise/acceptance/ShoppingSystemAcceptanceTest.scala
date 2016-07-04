115cfb0bf42db6b176231726b58121cfa753086f
guillaume <guillaume.delarue@gmail.com>
2016-07-04 17:46:56 +0100

    step 2: acceptance tests

--- src/test/scala/gd/hmrc/exercise/acceptance/ShoppingSystemAcceptanceTest.scala
+++ src/test/scala/gd/hmrc/exercise/acceptance/ShoppingSystemAcceptanceTest.scala
@@ -1,6 +1,6 @@
 package gd.hmrc.exercise.acceptance
 
-import gd.hmrc.exercise.{JunitFunSuite, ShoppingSystem}
+import gd.hmrc.exercise.{APPLE, JunitFunSuite, ORANGE, ShoppingSystem}
 
 class ShoppingSystemAcceptanceTest extends JunitFunSuite {
   private val shoppingSystem = new ShoppingSystem()
@@ -17,5 +17,23 @@ class ShoppingSystemAcceptanceTest extends JunitFunSuite {
       shoppingSystem.calculateTotalCost("Bunch of monkeys")
     }
     expectedException.getMessage must be(expectedMessage)
+  }
+
+  test("first promotion: buy one apple get one free") {
+    shoppingSystem.calculateTotalCost("Apple", "Apple") must be(APPLE.price)
+    shoppingSystem.calculateTotalCost("Apple", "Apple", "Apple") must be(2 * APPLE.price)
+    shoppingSystem.calculateTotalCost("Apple", "Apple", "Apple", "Apple") must be(2 * APPLE.price)
+    shoppingSystem.calculateTotalCost("Apple", "Apple", "Apple", "Apple", "Apple") must be(3 * APPLE.price)
+
+    shoppingSystem.calculateTotalCost("Apple", "Orange", "Apple") must be(APPLE.price + ORANGE.price)
+    shoppingSystem.calculateTotalCost("Apple", "Orange", "Apple", "Orange") must be(APPLE.price + 2 * ORANGE.price)
+  }
+
+  test("second promotion: three oranges for the price of two") {
+    shoppingSystem.calculateTotalCost("Orange", "Orange") must be(2 * ORANGE.price)
+    shoppingSystem.calculateTotalCost("Orange", "Orange", "Orange") must be(2 * ORANGE.price)
+    shoppingSystem.calculateTotalCost("Orange", "Orange", "Orange", "Orange") must be(3 * ORANGE.price)
+    shoppingSystem.calculateTotalCost("Orange", "Orange", "Orange", "Orange", "Orange") must be(4 * ORANGE.price)
+    shoppingSystem.calculateTotalCost("Orange", "Orange", "Orange", "Orange", "Orange", "Orange") must be(4 * ORANGE.price)
   }
 }
